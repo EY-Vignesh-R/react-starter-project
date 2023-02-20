2b7fcae17b13e5cbf47560418f85550a1f94223b
EY-Vignesh-R <Vignesh.R@gds.ey.com>
2023-02-20 10:40:50 +0530

    React Starter Class [17-02-23]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Child from './components/Child';
+// import Child from './components/Child';
 import Users from './components/Users';
 import Admin from './components/Admin';
 import { useState } from "react";
@@ -8,10 +8,10 @@ const [userData,setUserData]=useState(false)
 const [adminData,setAdminData]=useState(false)
 
   const users = [
-    { id: 1, name: "Virat",age:18,gender:'M',emailId:"alice@gmail.com" },
+    { id: 1, name: "Virat",age:18,gender:'M',emailId:"virat@gmail.com" },
     { id: 2, name: "Sachin",age:24,gender:'M',emailId:"sachin@gmail.com" },
     { id: 3, name: "Messi",age:30,gender:'M',emailId:"messi@gmail.com" },
-    { id: 4, name: "Saina",age:35,gender:'F',emailId:"saina@gmail.com" },
+    { id: 4, name: "Sindhu",age:35,gender:'F',emailId:"sindhu@gmail.com" },
     { id: 5, name: "Ronaldo",age:40,gender:'M',emailId:"ronaldo@gmail.com" }
   ];
   const admins = [
